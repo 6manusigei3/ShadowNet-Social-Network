615692dd7fb18cd5d3327e41a30efbcdc1dacfd9
Manu_Sigei <kipchumbaemmanuel061@gmail.com>
2026-03-25 18:22:30 +0300

    Implement MaxHeap class using heapq

--- structures/heap.py
+++ structures/heap.py
@@ -1,1 +1,16 @@
+import heapq
 
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+
+    def push(self, item, priority):
+        heapq.heappush(self.heap, (-priority, item))
+
+    def pop(self):
+        if self.heap:
+            priority, item = heapq.heappop(self.heap)
+            return item, -priority
+
+    def is_empty(self):
+        return len(self.heap) == 0
