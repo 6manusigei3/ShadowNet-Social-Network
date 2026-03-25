1b98d1b004a20b77f11ba2cccc06d44abab2d9b4
Manu_Sigei <kipchumbaemmanuel061@gmail.com>
2026-03-25 18:23:09 +0300

    Refactor friend recommendation using MaxHeap

--- system/recommendation_engine.py
+++ system/recommendation_engine.py
@@ -1,3 +1,5 @@
+from structures.heap import MaxHeap
+
 def recommend_friends(graph, user):
     friends = set(graph.get_friends(user))
     recommendations = {}
@@ -5,15 +7,16 @@ def recommend_friends(graph, user):
     for friend in friends:
         for mutual in graph.get_friends(friend):
             if mutual != user and mutual not in friends:
-                if mutual not in recommendations:
-                    recommendations[mutual] = 0
-                recommendations[mutual] += 1
+                recommendations[mutual] = recommendations.get(mutual, 0) + 1
 
-    # sort by number of mutual friends
-    sorted_recommendations = sorted(
-        recommendations.items(),
-        key=lambda x: x[1],
-        reverse=True
-    )
+    heap = MaxHeap()
 
-    return sorted_recommendations
+    for person, score in recommendations.items():
+        heap.push(person, score)
+
+    result = []
+
+    while not heap.is_empty():
+        result.append(heap.pop())
+
+    return result
