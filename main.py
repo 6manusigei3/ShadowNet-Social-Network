813ccbab91f5c85f298f8f1d531260442b25a554
Manu_Sigei <kipchumbaemmanuel061@gmail.com>
2026-03-25 18:14:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,8 @@ g = Graph()
 g.add_user("Alice")
 g.add_user("Bob")
 g.add_user("Carol")
-g.add_user("David")
 
 g.add_friend("Alice", "Bob")
-g.add_friend("Alice", "David")
 g.add_friend("Bob", "Carol")
 
 g.display()
