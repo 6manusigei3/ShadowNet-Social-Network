08e6003fd780c3caed220d7b7eedcff823dd2029
Manu_Sigei <kipchumbaemmanuel061@gmail.com>
2026-03-25 15:17:55 +0300

    Initialize graph and add users and friendships

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from structures.graph import Graph
 
+g = Graph()
+
+g.add_user("Alice")
+g.add_user("Bob")
+g.add_user("Carol")
+g.add_user("David")
+
+g.add_friend("Alice", "Bob")
+g.add_friend("Alice", "David")
+g.add_friend("Bob", "Carol")
+
+g.display()
