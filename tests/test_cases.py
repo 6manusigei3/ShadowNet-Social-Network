244b8fa095dc42a31ae1eac4afc73f1c596395ba
Manu_Sigei <kipchumbaemmanuel061@gmail.com>
2026-03-25 18:25:15 +0300

    Update test_cases.py

--- tests/test_cases.py
+++ tests/test_cases.py
@@ -1,1 +1,15 @@
+from system.friend_manager import FriendManager
 
+def run_tests():
+    fm = FriendManager()
+
+    fm.create_user("Alice")
+    fm.create_user("Bob")
+
+    fm.add_friend("Alice", "Bob")
+
+    assert "Bob" in fm.show_friends("Alice")
+
+    print("All tests passed!")
+
+run_tests()
