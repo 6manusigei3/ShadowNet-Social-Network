b6101ddd3dc502c0d4b5574a9e6aff64aaf0cbfc
Manu_Sigei <kipchumbaemmanuel061@gmail.com>
2026-03-25 15:21:57 +0300

    Implement FriendManager class for user management

--- system/friend_manager.py
+++ system/friend_manager.py
@@ -1,1 +1,20 @@
+from structures.graph import Graph
 
+class FriendManager:
+    def __init__(self):
+        self.graph = Graph()
+
+    def create_user(self, name):
+        self.graph.add_user(name)
+
+    def add_friend(self, user1, user2):
+        self.graph.add_friend(user1, user2)
+
+    def remove_friend(self, user1, user2):
+        self.graph.remove_friend(user1, user2)
+
+    def show_friends(self, user):
+        return self.graph.get_friends(user)
+
+    def display_network(self):
+        self.graph.display()
